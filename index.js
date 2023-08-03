f4513ea81239a5a6f0055ab42d45f19406d21fad
anshawasthi01 <anshawasthi3894@gmail.com>
2023-08-03 14:49:09 +0530

    ppa basic api deployment

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+// host some file
+
+// serve some requests
+// create our own server
+
+app.get('/', (req,res)=> {
+    res.send("Hello World");
+})
+
+// i will work for abcd and acd
+app.get('/a(bc)?d', (req,res)=> {
+
+    res.send("Hello World");
+    // changes that might resolve the bug
+})
+
+app.get('*', (req,res)=> {
+    res.send("404 not found");
+})
+
+app.listen("3001", () => {
+    console.log("Listening to this port : 3001");
+})
